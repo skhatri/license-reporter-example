d3ff92d1fe86ac8014c71d5de465b71a592cabe3
Suresh Khatri <servlet2@msn.com>
2022-02-05 01:32:40 +1100

    view sample report in docker

--- README.md
+++ README.md
@@ -9,5 +9,10 @@ gradle checkLicense
 
 
 
+#### View a sample report
+```
+docker run -p 8000:80 -it skhatri/oss-license-report-demo:latest
 
+open http://localhost:8000
+```
 
--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 ./gradlew clean gLR
 version=$(date +%Y%m%d%H%M)
 docker build --no-cache -t skhatri/oss-license-report-demo:${version} .
+docker tag skhatri/oss-license-report-demo:${version} skhatri/oss-license-report-demo:latest
 docker push skhatri/oss-license-report-demo:${version}
+docker push skhatri/oss-license-report-demo:latest
+
 
 
